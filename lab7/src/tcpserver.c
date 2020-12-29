fb9212761dedcadc91dc934e7860eaaadc77b850
nikitush <071299nik@mail.ru>
2020-12-29 16:36:07 +0000

    lab7

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -2,20 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
+//#define SERV_PORT 10050
+//#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
+int main(int argc, char *argv[]) {
   const size_t kSize = sizeof(struct sockaddr_in);
 
   int lfd, cfd;
   int nread;
+
+  int BUFSIZE = -1;
+  int SERV_PORT = -1;
+  while (1) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"port", required_argument, 0, 0},
+                                      {"bufsize", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case 0: {
+      switch (option_index) {
+      case 0:
+        SERV_PORT = atoi(optarg);
+        if (!(SERV_PORT>0))
+        return 0;
+        // TODO: your code here
+        break;
+      case 1:
+        BUFSIZE = atoi(optarg);
+        if (!(BUFSIZE>0))
+        return 0;
+        // TODO: your code here
+        break;
+      default:
+        printf("Index %d is out of options\n", option_index);
+      }
+    } break;
+
+    case '?':
+      printf("Unknown argument\n");
+      break;
+    default:
+      fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
+  if (SERV_PORT == -1 || BUFSIZE == -1) {
+    fprintf(stderr, "Using: %s --port 20001 --bufsize 4\n", argv[0]);
+    return 1;
+  }
+
   char buf[BUFSIZE];
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
